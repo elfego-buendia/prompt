1953bbf6bb8669977d5a6848659eb823c83e82ef
Jose Martinez <elfego.buendia.x@gmail.com>
2014-05-09 14:35:57 -0500

    changed us flag, cleaned up AEC class, added other ps pages

--- js/AEC.js
+++ js/AEC.js
@@ -7,46 +7,48 @@ function AEC(s) {
     var modi = null,
         colr = null,
         bkgc = null,
-        clss = [];
-    
-    function construct() {
-        var segmentArray = s.split(';'),
-            numberOfSegments = segmentArray.length,
-            firstSegment = null,
-            secondSegment = null,
-            thirdSegment = null;
+        clss = [],
+		segmentArray = s.split(';'),
+        numberOfSegments = segmentArray.length,
+        firstSegment = null,
+        secondSegment = null,
+        thirdSegment = null;
+			
+	// assign variables according to the number of modifiers present
+	// 3 = [bold/underline] + overall color + background color
+	// 2 = [bold/underline] + overall color
+	// 1 = background color only
+    if (numberOfSegments === 3) {
+        firstSegment = segmentArray[0];
+        secondSegment = segmentArray[1];
+        thirdSegment = segmentArray[2];
         
-        if (numberOfSegments === 3) {
-            firstSegment = segmentArray[0];
-            secondSegment = segmentArray[1];
-            thirdSegment = segmentArray[2];
-            
-            modi = MODIFI[firstSegment];
-            colr = COLORS[secondSegment[secondSegment.length - 1]];
-            bkgc = BGADD + COLORS[thirdSegment[thirdSegment.length - 2]];
-        } else if (numberOfSegments === 2) {
-            firstSegment = segmentArray[0];
-            secondSegment = segmentArray[1];
-            
-            modi = MODIFI[firstSegment];
-            colr = COLORS[secondSegment[secondSegment.length - 2]];
-        } else if (numberOfSegments === 1) {
-            thirdSegment = segmentArray[0];
-            bkgc = BGADD + COLORS[thirdSegment[thirdSegment.length - 2]];
-        }
+        modi = MODIFI[firstSegment];
+        colr = COLORS[secondSegment[secondSegment.length - 1]];
+        bkgc = BGADD + COLORS[thirdSegment[thirdSegment.length - 2]];
+		
+		clss.push(modi);
+		clss.push(colr);
+		clss.push(bkgc);
+    } else if (numberOfSegments === 2) {
+        firstSegment = segmentArray[0];
+        secondSegment = segmentArray[1];
         
-        if (modi) {
-            clss.push(modi);
-        }
-        if (colr) {
-            clss.push(colr);
-        }
-        if (bkgc) {
-            clss.push(bkgc);
-        }
+        modi = MODIFI[firstSegment];
+        colr = COLORS[secondSegment[secondSegment.length - 2]];
+		
+		clss.push(modi);
+		clss.push(colr);
+    } else if (numberOfSegments === 1) {
+        thirdSegment = segmentArray[0];
+        bkgc = BGADD + COLORS[thirdSegment[thirdSegment.length - 2]];
+		
+		clss.push(bkgc);
+    } else {
+    	console.log('Error creating AEC object.')
     }
-    construct();
     
+	// methods to access private variables (read only)
     this.getModi = function () {
         return modi;
     };
